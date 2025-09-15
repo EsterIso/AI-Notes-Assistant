0ee2a46dede4f9a6a94dc2fdb9cbec99274c7c2b
EsterIso <esterlinjpaulino@gmail.com>
2025-09-15 01:18:24 -0400

    cleanup: removed old React+Vite client and server folders

--- server/models/quizQuestion.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const quizQuestionSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  choices: [{ type: String, required: true }],
-  answer: { type: String, required: true },
-});
-
-export default mongoose.model("QuizQuestion", quizQuestionSchema);
